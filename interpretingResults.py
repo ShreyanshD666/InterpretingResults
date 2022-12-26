4d0c2e5bd1e9115b0155729d0a675af405a32010
ShreyanshD666 <71385954+ShreyanshD666@users.noreply.github.com>
2022-12-26 22:37:23 +0530

    Update interpretingResults.py

--- interpretingResults.py
+++ interpretingResults.py
@@ -6,28 +6,28 @@ df.head()
 
 df.drop(['Unnamed: 0'],axis =1,inplace=True)
 
-name = df["StarName"].to_list()
+name = df["Star_name"].to_list()
 mass = df["Mass"].to_list()
 radius = df["Radius"].to_list()
 dist = df["Distance"].to_list()
 gravity = df["Gravity"].to_list()
 
 plt.title("Star Name vs Mass")
-plt.xlabel("StarName")
+plt.xlabel("Star_name")
 plt.ylabel("Mass")
 plt.show()
 
 plt.title("Star Name vs Radius")
-plt.xlabel("StarName")
+plt.xlabel("Star_name")
 plt.ylabel("Radius")
 plt.show()
 
 plt.title("Star Name vs Distance")
-plt.xlabel("StarName")
+plt.xlabel("Star_name")
 plt.ylabel("Distance")
 plt.show()
 
 plt.title("Star Name vs Gravity")
-plt.xlabel("StarName")
+plt.xlabel("Star_name")
 plt.ylabel("Gravity")
 plt.show()
